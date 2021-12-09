ca71d8ff943852b5bfa97c7c58194fde65a70f93
j-matt <jmatt.technology@gmail.com>
2021-12-09 20:53:38 +0100

    skeleton of resource server

---
+++ spring-security-keycloak/spring-security-resource-server/src/test/java/com/jmatt/security/rs/SpringSecurityResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmatt.security.rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
